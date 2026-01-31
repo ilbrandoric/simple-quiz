31102aa4b2a862f6202072e45cb2cb0999ffa848
ilbrandoric <240859022+ilbrandoric@users.noreply.github.com>
2026-01-31 22:06:14 +0100

    fixed CSS code

--- script.js
+++ script.js
@@ -1,103 +1,119 @@
-// === Constant ===
-
-// Targets the start button & call the startGame function on click
+// Targeting DOM elements
 
 const startButton = document.getElementById("start-button");
-startButton.addEventListener("click", startGame);
-
 const nextButton = document.getElementById("next-button");
-nextButton.addEventListener("click", () => {
-  selectNext();
-});
 
-// HTML Targets 
+const questionContainer = document.getElementById("question-container");
+const answerButtons = document.getElementById("answer-buttons");
 
-const questionContainerElement = document.getElementById("question-container");
-const questionElement = questionContainerElement;
-
-const answerButtonelement = document.getElementById("answer-buttons");
-const answerElement = answerButtonelement;
-
-// Suffler array and index
+// STATE (what situation is the program currently in?)
 
 let shuffledQuestions = [];
 let currentQuestionIndex = 0;
 
-// ===== Control ====
+// Start / Restart buttons
 
-// Start the game function
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", showNextQuestion);
+
+// Game flow
 
 function startGame() {
-  console.log("Quiz started ...");
+  // Hides start button once game starts
   startButton.classList.add("hidden");
-  questionContainerElement.classList.remove("hidden");
 
-  answerButtonelement.classList.remove("hidden");
+  // Unhides question & answer buttons
+  questionContainer.classList.remove("hidden");
+  answerButtons.classList.remove("hidden");
 
-  shuffledQuestions = quizQuestions.sort(() => Math.random() - 0.5);
-  showQuestion(shuffledQuestions[currentQuestionIndex]);
+  // Resets the value so if game is a restart it start 'clean' with no bugs
+  currentQuestionIndex = 0;
+
+  // 'Cheap' trick to randomize questions and STORE in shuffledQuestions array
+  shuffledQuestions = quizQuestions.sort(() => Math.random() - 0.5); //gives a random number between -0.5 and +0.5
+
+  showQuestion();
 }
 
-function selectAnswer(e) {
-  const selectedButton = e.target;
-  const correct = selectedButton.dataset.correct === "true";
-  setStatusClass(document.body, correct);
-  Array.from(answerButtonelement.children).forEach((button) => {
-    setStatusClass(button, button.dataset.correct === "true");
-  });
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove("hidden");
-  } else {
-    startButton.innerText = "Restart";
-    startButton.classList.remove("hidden");
-    nextButton.classList.add("hidden");
-  }
-}
+// Question logic
 
-function setStatusClass(element, correct) {
-  clearStatusclass(element);
-  if (correct) {
-    element.classList.add("correct");
-  } else {
-    element.classList.add("incorrect");
-  }
-}
-
-function clearStatusclass(element) {
-  element.classList.remove("correct");
-  element.classList.remove("incorrect");
-}
-
-function selectNext() {
-  if (currentQuestionIndex + 1 < shuffledQuestions.length) {
-    currentQuestionIndex++;
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-  }
-}
-
-function showQuestion(quizQuestions) {
+function showQuestion() {
   resetState();
 
-  questionElement.innerText = quizQuestions.question;
+  const currentQuestion = shuffledQuestions[currentQuestionIndex];
+  questionContainer.innerText = currentQuestion.question;
 
-  quizQuestions.options.forEach((option) => {
+  // Loops inside each questions answer options and displays it
+  currentQuestion.options.forEach((option) => {
     const button = document.createElement("button");
     button.innerText = option.text;
     button.classList.add("btn");
 
+    // You’re attaching custom data directly to the button (correct = true)
+    // The browser stores data-correct="true"
+
     button.dataset.correct = option.correct;
 
     button.addEventListener("click", selectAnswer);
-    answerButtonelement.appendChild(button);
+    // Adds an answer option as a button under id="answer-buttons" or
+    // insert this button as a child of the answerButtons container.
+    answerButtons.appendChild(button);
   });
 }
 
-function resetState() {
-  nextButton.classList.add("hidden");
-  while (answerButtonelement.firstChild) {
-    answerButtonelement.removeChild(answerButtonelement.firstChild);
+function showNextQuestion() {
+  currentQuestionIndex++;
+  showQuestion();
+}
+
+// Answer logic
+
+function selectAnswer(e) {
+  // This is the answer the user chose 'e.target' or the button the user clicked
+  const selectedButton = e.target;
+  // Is the option correct?
+  const isCorrect = selectedButton.dataset.correct === "true";
+
+  // Color right answer green — even if you clicked wrong answer.
+  Array.from(answerButtons.children).forEach((button) => {
+    setStatus(button, button.dataset.correct === "true");
+  });
+
+  // Are there more questions left in the deck? Yes > Show next.
+  if (currentQuestionIndex < shuffledQuestions.length - 1) {
+    nextButton.classList.remove("hidden");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hidden");
   }
 }
+
+// Clean UI
+
+function setStatus(element, correct) {
+  clearStatus(element);
+  // If answer is correct change CSS to correct (color green) else incorrect (color red)
+  element.classList.add(correct ? "correct" : "incorrect");
+}
+
+// Clears any CSS (color) from answer
+function clearStatus(element) {
+  element.classList.remove("correct");
+  element.classList.remove("incorrect");
+}
+
+// Clears questions and answers from being displayed
+function resetState() {
+  // Hides next Button
+  nextButton.classList.add("hidden");
+
+  // Translation: while a first button exist or is TRUE then remove
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+// Questions
 
 const quizQuestions = [
   {
